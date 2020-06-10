e63d58901c143e8d2e70181ec71a34d9753de04a
luifap <52289372+luifap@users.noreply.github.com>
2020-06-10 14:47:45 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Prints "Programming is like building a multilingual puzzle"
+ * Return: 0 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"); /*string*/
 	return (0);
 }
