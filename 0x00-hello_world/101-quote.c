efd3a09b91ec91fe2ed680a51cc6fa2fcfd5207a
luifap <52289372+luifap@users.noreply.github.com>
2020-06-10 21:22:09 -0500

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    fprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 21);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
