bc9a2277c1006a4715a31074ba680bba1c24b078
luifap <52289372+luifap@users.noreply.github.com>
2020-06-11 18:56:39 -0500

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = n; n < 10; n++)
+	{
+		putchar(n);
+	}
+putchar('\n');
+	return (0);
+}
