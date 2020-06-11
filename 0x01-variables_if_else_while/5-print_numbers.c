051e31fc9221eeabe8fec6d7bf89ce5e8990aa67
luifap <52289372+luifap@users.noreply.github.com>
2020-06-11 18:23:08 -0500

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int numero;
+
+    numero = 0;
+    printf( "\n   %d ", numero );  /* Escribe el 1 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 2 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 3 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 4 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 5 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 6 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 7 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 8 */
+    numero++;
+    printf( "%d ", numero );  /* Escribe el 9 */
+    numero++;
+    printf( "%d", numero );  /* Escribe el 10 */
+
+    getch(); /* Pausa */
+
+    return 0;
+}
