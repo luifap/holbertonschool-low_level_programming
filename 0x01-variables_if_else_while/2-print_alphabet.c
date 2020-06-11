635d1b43821a099cf5fd18b38cb23802545534f0
luifap <52289372+luifap@users.noreply.github.com>
2020-06-11 16:49:52 -0500

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++c)
+  putchar(a);
+	return (0);
+}
